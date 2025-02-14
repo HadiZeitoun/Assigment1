7732e49b8cc10b1a4a19c9689a1fe811bd0c11b8
Hadi Zeitoun <Hadii_Zeitoun@hotmail.com>
2025-02-14 16:27:50 +0200

    Solved exercise 3

---
+++ Exercise3.py
@@ -0,0 +1,10 @@
+
+#Number 3:
+username="admin"
+password="1234"
+user=input("enter your username: ")
+passw=input("enter your password: ")
+if user==username and passw==password:
+	print("Access granted")
+else:
+	print("Acces denied")
